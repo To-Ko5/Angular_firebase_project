6f44b0737f82603cf57f31544df5782a3f0aae69
To-Ka <kobayashitoshifumi@kobayashitoshifuminoMacBook-Air.local>
2020-02-22 21:37:42 +0900

    feat(all):clear caced

--- functions/src/git-hook.function.ts
+++
@@ -1,6 +0,0 @@
-import * as functions from "firebase-functions";
-
-export const gitHook = functions.https.onRequest((request, response) => {
-  console.log(request.body); //demo pushed4
-  response.send("succes");
-});
--- src/app/interfaces/pet.ts
+++
@@ -1,8 +0,0 @@
-export interface Pet {
-  petImageId: number;
-  name: string;
-  level: number;
-  exp: number;
-  trainerId: string;
-  gender: 'male' | 'female'
-}
