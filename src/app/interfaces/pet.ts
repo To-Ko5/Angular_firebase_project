c849e280e0e7a7b99a3f5c2d6b6b1b56fe0949a3
To-Ka <kobayashitoshifumi@kobayashitoshifuminoMacBook-Air.local>
2020-02-22 21:30:02 +0900

    feat(all):project create

---
+++ src/app/interfaces/pet.ts
@@ -0,0 +1,8 @@
+export interface Pet {
+  petImageId: number;
+  name: string;
+  level: number;
+  exp: number;
+  trainerId: string;
+  gender: 'male' | 'female'
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { auth, User } from "firebase/app";
+import { Observable } from "rxjs";
+import { Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthService {
+  afUser$: Observable<User> = this.afAuth.user;
+
+  uid: string;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {
+    this.afUser$.subscribe(user => {
+      this.uid = user ? user.uid : null;
+    });
+  }
+
+  login() {
+    this.afAuth.auth
+      .signInWithPopup(new auth.GithubAuthProvider())
+      .then(result => {
+        this.snackBar.open("ようこそ😄", null, {
+          duration: 3000
+        });
+        this.router.navigateByUrl("/create");
+      });
+  }
+
+  logout() {
+    this.afAuth.auth.signOut().then(() => {
+      this.snackBar.open("ログアウトしました。", null, {
+        duration: 3000
+      });
+    });
+    this.router.navigateByUrl("/welcome");
+  }
+}
