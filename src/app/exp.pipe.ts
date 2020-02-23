bf7cddad2f96d8311fd05819aa361da374aa71a8
To-Ka <kobayashitoshifumi@kobayashitoshifuminoMacBook-Air.local>
2020-02-23 13:41:21 +0900

    feat(exp-pipe):パーセント表示の調節

--- src/app/exp.pipe.ts
+++ src/app/exp.pipe.ts
@@ -1,33 +1,23 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { Pet } from './interfaces/pet';
+import { Pipe, PipeTransform } from "@angular/core";
+import { Pet } from "./interfaces/pet";
 
-const expTable = [
-  20,
-  40,
-  100,
-  150,
-  250,
-  500,
-  1000,
-  1500,
-  2500,
-  5000
-]
-
+const expTable = [20, 40, 100, 150, 250, 500, 1000, 1500, 2500, 5000];
 
 @Pipe({
-  name: 'exp'
+  name: "exp"
 })
 export class ExpPipe implements PipeTransform {
-
-  transform(pet:Pet, ...args: any[]): any {
+  transform(pet: Pet, type?: 'percent' | 'label'): any {
     const totalExp = pet.exp;
     const level = pet.level;
     const baseExp = expTable[level - 2] || 0;
     const nextExp = expTable[level - 1] - baseExp;
     const exp = totalExp - baseExp;
-    return exp + '/' + nextExp;
 
+    if (type === 'percent') {
+      return (exp / nextExp) * 100;
+    } else {
+      return exp + "/" + nextExp;
+    }
   }
-
 }
